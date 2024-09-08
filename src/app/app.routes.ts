6e7f1001c2e0e5e26036a82d6955192737800815
Rahullama946 <rahullama157@gmail.com>
2024-09-08 21:58:24 +0545

    make a new sample app with simple routing features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'crisis',component:CrisisListComponent
+    },
+    {
+        path:"heroes",component:HeroesListComponent
+    },
+    {
+        path:'',redirectTo:'/heroes',pathMatch:'full'
+    },
+    {
+        path:"**",component : NotFoundPageComponent
+    },
+    
+];
